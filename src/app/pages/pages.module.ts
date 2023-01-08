751ef1275cc7df7651252e00b710300ef78c177b
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-01-08 18:52:47 -0300

    Conectando página de logo Poke-header a home

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 //Own Modules
 import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 //Components
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
