4612b8e0dfde2547286ceddea6ce4c07aaa80960
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-01-12 10:42:08 -0300

    Criando service e carregando a api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 // Own Modules
 import { PagesModule } from './pages/pages.module';
@@ -15,7 +16,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
